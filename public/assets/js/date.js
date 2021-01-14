47e37ca8581dfbd6f95bbc2cd7a6ca47824042ef
Improbdrive <sam.wakelam@hotmail.com>
2021-01-14 20:13:06 +0000

    getting day id back from the calander

---
+++ public/assets/js/date.js
@@ -0,0 +1,19 @@
+
+function getDateData(){
+
+  // set up elements from html
+  const currentYear = $('#current-year');
+  const currentMonth = $('#current-month');
+
+  // create the calander bar date content
+
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = date.getMonth();
+
+  // const monthDays = daysInMonth(year, month);
+  const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+  return [currentMonth, currentYear, date, year, month, monthNames, weekDays];
+}
