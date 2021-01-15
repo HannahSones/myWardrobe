6c633ca8f49ede1f02c8f80a32ec54244dd348fe
Corey Samuels <coreyjsamuels@gmail.com>
2021-01-15 01:53:04 +0000

    Adding Test HTML page and adding upload image route

---
+++ routes/upload.js
@@ -0,0 +1,37 @@
+const fileupload = require("express-fileupload");
+const cloudinary = require("cloudinary").v2;
+const streamifier = require("streamifier");
+const multer = require("multer");
+const ck = require("ckey");
+const cloud_name = ck.cloud_name;
+const api_key = ck.api_key;
+const api_secret = ck.api_secret;
+
+const upload = multer();
+
+const router = require("./item");
+cloudinary.config({
+  cloud_name: cloud_name,
+  api_key: api_key,
+  api_secret: api_secret,
+});
+
+router.post("/", upload.single("image"), (req, res) => {
+  console.log(req.files);
+  const cld_upload_stream = cloudinary.uploader.upload_stream(
+    {
+      folder: "myWardrobe",
+    },
+    (err, result) => {
+      if (err)
+        return res.status(500).json({
+          success: false,
+          payload: { message: "Unable to upload image" },
+        });
+      return res.json({ success: true, payload: result });
+    }
+  );
+  streamifier.createReadStream(req.files.image.data).pipe(cld_upload_stream);
+});
+
+module.exports = router;
