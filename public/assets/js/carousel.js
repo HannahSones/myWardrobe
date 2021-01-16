561d36912b343698da1b9962614fe23387b646e5
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-01-16 10:32:37 +0000

    All items showing in carousel

--- public/assets/js/carousel.js
+++ public/assets/js/carousel.js
@@ -1,13 +1,13 @@
-// Initiates carousel scrolling
-$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+// Initiates carousel scrolling, 1 item at a time
+$('.carousel.carousel-multi-item .carousel-item').each(function(){
     let next = $(this).next();
     if (!next.length) {
       next = $(this).siblings(':first');
     }
     next.children(':first-child').clone().appendTo($(this));
   
-    for (let i=0;i<3;i++) {
-      next=next.next();
+    for (let i=0; i<3; i++) {
+      next = next.next();
       if (!next.length) {
         next = $(this).siblings(':first');
       }
