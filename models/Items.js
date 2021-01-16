1b8d159bd2b2208935c989795b5371099f6e0706
Corey Samuels <coreyjsamuels@gmail.com>
2021-01-16 12:22:38 +0000

    upload array

--- models/Items.js
+++ models/Items.js
@@ -1,36 +1,35 @@
-const Sequelize = require('sequelize');
-const db = require('../config/database.js');
+const Sequelize = require("sequelize");
+const db = require("../config/database.js");
 
-const Item = db.define('item', {
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true
-    },
-    outfit_id: {
-        type: Sequelize.INTEGER
-    },
-    type: {
-        type: Sequelize.STRING
-    },
-    name: {
-        type: Sequelize.STRING
-    },
-    img: {
-        type: Sequelize.STRING
-    },
-    category: {
-        type: Sequelize.STRING
-    },
-    createdAt: {
-        type: Sequelize.DATE
-    },
-    updatedAt: {
-        type: Sequelize.DATE
-    }
+const Item = db.define("item", {
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+  },
+  outfit_id: {
+    type: Sequelize.INTEGER,
+  },
+  type: {
+    type: Sequelize.STRING,
+  },
+  name: {
+    type: Sequelize.STRING,
+  },
+  img: {
+    type: Sequelize.STRING,
+  },
+  category: {
+    type: Sequelize.STRING,
+  },
+  createdAt: {
+    type: Sequelize.DATE,
+  },
+  updatedAt: {
+    type: Sequelize.DATE,
+  },
 });
 
 module.exports = Item;
-
 
 /* outfit_id: '',
     type: '',
