35726114247f5d1cf301688ed3e4c193cf889b9d
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-01-16 12:57:43 +0000

    Three working carousels

--- public/assets/js/carousel.js
+++ public/assets/js/carousel.js
@@ -1,16 +1,91 @@
-// Initiates carousel scrolling, 1 item at a time
-$('.carousel.carousel-multi-item .carousel-item').each(function(){
-    let next = $(this).next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-  
-    for (let i=0; i<3; i++) {
-      next = next.next();
-      if (!next.length) {
-        next = $(this).siblings(':first');
-      }
-      next.children(':first-child').clone().appendTo($(this));
-    }
-  });
+// shorthand for document.ready function
+$(function() {
+
+// Initiates carousel for tops, scrolling one at a time
+    $('.topsCarousel').slick({
+        centerMode: true,
+        centerPadding: '150px',
+        slidesToShow: 5,
+        autoplay: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+
+
+// Initiates carousel for bottoms, scrolling one at a time
+      $('.bottomsCarousel').slick({
+        centerMode: true,
+        centerPadding: '150px',
+        slidesToShow: 5,
+        autoplay: true,
+        rtl: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
+
+
+// Initiates carousel for overalls, scrolling one at a time
+      $('.overallsCarousel').slick({
+        centerMode: true,
+        centerPadding: '150px',
+        slidesToShow: 5,
+        autoplay: true,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+    });
+
+});
