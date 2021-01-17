40767458014a15f13816d305f609b5bc975cafe2
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-01-17 01:35:31 +0000

    Form built for add item ready to like to db

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   // import date elements from date.js
-  const {currentMonth, currentYear, monthNames} = getDateData();
+  const { currentMonth, currentYear, monthNames } = getDateData();
 
   // set up elements from html
   const calendarDay = $('#calendar-row');
@@ -11,14 +11,14 @@ $(document).ready(function(){
 
   // FUNCTIONS
 
-  function getDayID(){
+  function getDayID() {
 
     const text = $(this).text().split(' ');
     let textDate = text[1];
     const textMonth = currentMonth.text();
     const textYear = currentYear.text();
 
-    if(textDate.length === 1){
+    if (textDate.length === 1) {
       const zero = '0';
       textDate = zero + textDate;
       // console.log("textDate =", textDate, typeof(textDate));
@@ -28,7 +28,7 @@ $(document).ready(function(){
     const monthIndex = monthNames.indexOf(textMonth) + 1;
     let valueMonth = monthIndex.toString();
     // console.log("valueMonth =", valueMonth, typeof(valueMonth));
-    if(valueMonth.length === 1){
+    if (valueMonth.length === 1) {
       const zero = '0';
       valueMonth = zero + valueMonth;
       // console.log("valueMonth =", valueMonth, typeof(valueMonth));
@@ -46,22 +46,25 @@ $(document).ready(function(){
 
 
 
-  // function for displaying categories based on type selection
+  // function for displaying categories based on type selection in Add Item form
 
-  $("select").change(function() {
+  $("select").change(function () {
     const selection = document.getElementById("typeSelection").value;
-  if (selection === "Top"){
-    $("topTypeSelected").css("display", "block");
-  }
-  else if (selection === "Bottom"){
-    $("bottomTypeSelected").css("display", "block");
-  }
-  else if (selection === "Overall"){
-    $("overallTypeSelected").css("display: block");
-  };
-
-  console.log(selection);
-
+    if (selection === "Top") {
+      $("#topTypeSelected").css("display", "block");
+      $("#bottomTypeSelected").css("display", "none");
+      $("#overallTypeSelected").css("display", "none");
+    }
+    else if (selection === "Bottom") {
+      $("#topTypeSelected").css("display", "none");
+      $("#bottomTypeSelected").css("display", "block");
+      $("#overallTypeSelected").css("display", "none");
+    }
+    else if (selection === "Overall") {
+      $("#topTypeSelected").css("display", "none");
+      $("#bottomTypeSelected").css("display", "none");
+      $("#overallTypeSelected").css("display", "block");
+    };
   });
 
 
