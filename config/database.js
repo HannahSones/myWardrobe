13258a5c29efb90a4a7510f095380c3d11e0662e
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-01-21 18:30:44 +0000

    using JAWSDB

--- config/database.js
+++ config/database.js
@@ -3,13 +3,26 @@ const ck = require('ckey');
 
 const password = ck.DB_PASSWORD;
 
-module.exports = new Sequelize('sams-wardrobe', 'root', password, {
-  host: 'localhost',
-  dialect: 'mysql',
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+// module.exports = new Sequelize('sams-wardrobe', 'root', password, {
+//   host: 'localhost',
+//   dialect: 'mysql',
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000,
+//   },
+// });
+
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: password,
+    dialect: 'mysql',
+    database: 'sams-wardrobe'
+  });
+}; 
